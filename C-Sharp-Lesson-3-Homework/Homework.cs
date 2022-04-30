e4c1c0a04ed15464a1651cd2ddbe03f47f301da5
MelnicElisaveta <headphones2022.outlook.com>
2022-04-30 18:08:01 +0300

    Homework done final

--- C-Sharp-Lesson-3-Homework/Homework.cs
+++ C-Sharp-Lesson-3-Homework/Homework.cs
@@ -58,7 +58,14 @@ namespace C_Sharp_Lesson_3_Homework
              *    
              */
             //your code here
-            int sum = 0;
+            int sum1 = 0;
+            int sum2 = 0;
+
+            if (array.GetLength(0) != array.GetLength(1))
+            {
+                Console.WriteLine("This matrix doesn't have a diagonal");
+                return;
+            }
 
             for (int row = 0; row < array.GetLength(0); row++)
             {
@@ -66,12 +73,16 @@ namespace C_Sharp_Lesson_3_Homework
                 {
                     if (row == column)
                     {
-                        sum += array[row, column];
+                        sum1 += array[row, column];
                     }
-                    else sum = 0;
+                    if (array.GetLength(1) - column - 1 == row)
+                    {
+                        sum2 += array[row, column];
+                    }
                 }
             }
-            Console.WriteLine("Diagonal Array Element Sum : " + sum);
+            Console.WriteLine("First Diagonal : " + sum1);
+            Console.WriteLine("Second Diagonal : " + sum2);
         }
 
         public void StarPrinter(int triangleHight)
@@ -83,32 +94,58 @@ namespace C_Sharp_Lesson_3_Homework
              *          * * * 
              */
             //your code here
+            for (int row = 0; row < triangleHight; row++)
+            {
+                for (int column = 0; column < (triangleHight * 2 - 1); column++)
+                {
+                    if (row == triangleHight - 1 || row + triangleHight - 1 == column || row == (triangleHight - column - 1))
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.Write("\n");
+            }
         }
-        public void SortList(IList<int> listOfNumbers)
-        {
-            //Print to console elements of  listOfNumbers in ascending order
-            //your code here
+
+            public void SortList(IList<int> listOfNumbers)
+            {
+                //Print to console elements of  listOfNumbers in ascending order
+                //your code here
+                var sortedList = new List<int>(listOfNumbers);
+                sortedList.Sort();
+
+                foreach (int item in sortedList)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+
+                Console.ReadKey();
+            }
         }
-        public static void Main(String[] args)
-        {
-            Homework homework = new Homework();
-            IList<int> list = new List<int>() { -5, 8, -7, 0, 44, 121, -7 };
-            int[,] matrix = new int[3, 3] {
+
+            public static void Main(String[] args)
+            {
+                Homework homework = new Homework();
+                IList<int> list = new List<int>() { -5, 8, -7, 0, 44, 121, -7 };
+                int[,] matrix = new int[3, 3] {
                 { 1,   3,  5},
                 { 2, 3, 5},
                 {100, 56 , -54} };
-            int[,] matrix2 = new int[3, 4] {
+                int[,] matrix2 = new int[3, 4] {
                 { 1,   3,  5,   6},
                 { 2,   3,  5,  78},
                 {100, 56 , -54, 6} };
 
-            homework.GetCentralElementFromMatrix(matrix);
-            homework.GetCentralElementFromMatrix(matrix2);
-            homework.GetSummOfDiagonalsElements(matrix);
-            homework.GetSummOfDiagonalsElements(matrix2);
-            homework.StarPrinter(5);
-            homework.SortList(list);
-        }
+                homework.GetCentralElementFromMatrix(matrix);
+                homework.GetCentralElementFromMatrix(matrix2);
+                homework.GetSummOfDiagonalsElements(matrix);
+                homework.GetSummOfDiagonalsElements(matrix2);
+                homework.StarPrinter(5);
+                homework.SortList(list);
+            }
 
-    }
 }
